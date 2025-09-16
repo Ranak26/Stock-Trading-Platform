d160edbd8f53c04438cc20420388683c59e9a2b0
Ranak <ranakpradhan55@gmail.com>
2025-09-16 14:49:24 +0530

    support page

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -6,13 +6,12 @@ import App from './App.jsx'
 import Homepage from './Landing_Page/Home/HomePage.jsx'
 import Signup from './Landing_Page/Signup/Signup.jsx'
 import About from './Landing_Page/About/AboutPage.jsx'
-import Pricing from './Landing_Page/Pricing/Hero.jsx'
-import Product from './Landing_Page/Products/Hero.jsx'
-import Support from './Landing_Page/Support/Hero.jsx'
 import Navbar from './Landing_Page/Navbar.jsx'
 import Footer from './Landing_Page/Footer.jsx'
 import NotFound from './Landing_Page/NotFound.jsx'
 import ProductPage from './Landing_Page/Products/ProductPage.jsx'
+import PricingPage from './Landing_Page/Pricing/PricingPage.jsx'
+import SupportPage from './Landing_Page/Support/SupportPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
@@ -22,8 +21,8 @@ createRoot(document.getElementById('root')).render(
     <Route path='/signup' element={<Signup/>}></Route>
     <Route path='/about' element={<About/>}></Route>
     <Route path='/products' element={<ProductPage/>}></Route>
-    <Route path='/pricing' element={<Pricing/>}></Route>
-    <Route path='/support' element={<Support/>}></Route>
+    <Route path='/pricing' element={<PricingPage/>}></Route>
+    <Route path='/support' element={<SupportPage/>}></Route>
     <Route path='*' element={<NotFound/>}></Route>
   </Routes>
   <Footer/>
